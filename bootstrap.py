5322cf4bb7f5d246eb265462dbfe4844f0e069a3
Ubuntu <ubuntu@ip-172-31-13-90.us-west-2.compute.internal>
2014-02-12 21:33:20 +0000

    moved index to templates and made it be served when requested

--- bootstrap.py
+++ bootstrap.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<span style='color:yellow'>I am app 1</span>"
+	return render_template('index.html')
