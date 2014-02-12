11748046e1d7870a7428190df78b539eb6ac666a
Ubuntu <ubuntu@ip-172-31-13-90.us-west-2.compute.internal>
2014-02-12 21:57:10 +0000

    fixed seriving of index to work with proper server config

--- bootstrap.py
+++ bootstrap.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from flask import render_template
+from flask import redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return redirect('index.html')
