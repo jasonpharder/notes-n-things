6ee80f87683dad0cb5572df7fa8b2c84df1d6799
Jason Harder <jasonpharder@gmail.com>
2014-02-12 16:43:47 -0600

    added stub routing to bootstrap

--- bootstrap.py
+++ bootstrap.py
@@ -1,8 +1,21 @@
 from flask import Flask
-from flask import redirect
+from flask import redirect, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return redirect('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+	if request.method == 'POST':
+		#do login stuff here
+
+@app.route('/note/<name>')
+def show_notes(name):
+	#notes stuff here
+
+@app.route('/course/<name>')
+def show_course(name):
+	#courses stuff here
