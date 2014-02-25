768f69de3f2d4b28600e4547304a54882541de29
Jason Harder <jasonpharder@gmail.com>
2014-02-25 16:30:25 -0600

    notes layout rough work

--- public/js/app.js
+++ public/js/app.js
@@ -5,3 +5,15 @@ App.Router.map(function() {
   this.resource('courses');
   this.resource('login');
 });
+
+App.NotesRoute = Ember.Route.extend({
+	model: function() {
+		return [{
+			title: "test tile",
+			id: '1'
+		}, {
+			title: "test title 2",
+			id: '2'
+		}];
+	}
+});
