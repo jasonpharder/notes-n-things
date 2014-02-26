5e21d122c46fcecd004c8e70072f5899b54db4e7
Kyle Derrett <kylederrett@wpa-4-1909.cc.umanitoba.ca>
2014-02-26 11:49:35 -0600

    Added ios app

---
+++ iosApp/notes-n-things/notes-n-thingsTests/notes_n_thingsTests.m
@@ -0,0 +1,32 @@
+//
+//  notes_n_thingsTests.m
+//  notes-n-thingsTests
+//
+//  Created by Kyle Derrett on 2/26/2014.
+//  Copyright (c) 2014 Comp4350. All rights reserved.
+//
+
+#import "notes_n_thingsTests.h"
+
+@implementation notes_n_thingsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in notes-n-thingsTests");
+}
+
+@end
