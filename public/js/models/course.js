03e5da41eeda3d23dbd5cf191a5b597744f3c223
Jason Harder <jasonpharder@gmail.com>
2014-02-26 21:23:46 -0600

    made rough courses template

--- public/js/models/course.js
+++ public/js/models/course.js
@@ -1,27 +1,27 @@
-App.Note = DS.Model.extend({
+App.Course = DS.Model.extend({
 	title:    DS.attr('string'),
 	uid:      DS.attr('string'),
 	contents: DS.attr('string')
-});
+})
 
-//Stub data for notes
-App.Note.FIXTURES = [
+//stub Data for courses
+App.Course.FIXTURES = [
 	{
 		uid:      "1",
 		id :      1,
-		title:    "Test Note",
+		title:    "Test Course",
 		contents: "test content"
 	},
 	{
 		uid:      "2",
 		id :      2,
-		title:    "Test Note 2",
+		title:    "Test Course 2",
 		contents: "test content 2"
 	},
 	{
 		uid:      "3",
 		id :      3,
-		title:    "Test Note 3",
+		title:    "Test Course 3",
 		contents: "test content 3"
 	},
-];
+]
--- public/js/router.js
+++ public/js/router.js
@@ -2,8 +2,22 @@ App.Router.map(function() {
 	this.resource('notes', function() {
 		this.resource('note', { path : ':note_id' } );
 	});
-	this.resource('courses');
+	this.resource('courses', function() {
+		this.resource('course', { path : ':course_id' } );
+	});
 	this.resource('login');
+});
+
+App.CoursesRoute = Ember.Route.extend({
+	model: function() {
+		return this.store.find('course');
+	}
+});
+
+App.CourseRoute = Ember.Route.extend({
+	model: function(params) {
+		return this.store.find('course', params.course_id);
+	}
 });
 
 App.NotesRoute = Ember.Route.extend({
