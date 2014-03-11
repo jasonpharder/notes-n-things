a3b7bfc53f19703bc6300ebaf56465bc9d668b29
Jason Harder <jasonpharder@gmail.com>
2014-03-11 18:09:47 -0500

    added stub emebr application to tests

--- public/js/tests.js
+++ public/js/tests.js
@@ -1,3 +1,24 @@
+// put app in qunit-fixture
+window.App = Ember.Application.create({
+  rootElement: '#qunit-fixture'
+});
+
+// turn on testing mode
+window.App.setupForTesting();
+
 test( "hello test", function() {
   ok( 1 == "1", "Passed!" );
 });
+
+test("App Instantiation", function() {
+  equal(
+    App.constructor, Ember.Application,
+    'App is an Ember App!'
+  );
+});
+
+//test( "", function () {
+//	var courses = this.store.find('course');/
+//	alert(courses);
+//	ok( 1== "1", "Passed!");
+//})
