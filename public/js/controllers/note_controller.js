1e0937f2d6c990a80925ffc631c6a0b1b18d22a9
Jason Harder <jasonpharder@gmail.com>
2014-03-11 22:24:52 -0500

    added view toggle for editing notes

--- public/js/controllers/note_controller.js
+++ public/js/controllers/note_controller.js
@@ -1,8 +1,14 @@
-
 App.NotesController = Ember.ArrayController.extend({
-  actions: {
-    createNote: function () {
-      
-    }
-  }
+	actions: {
+		editNote: function () {
+    		this.set('isEditing')
+		},
+		doneEditing: function() {
+			this.set('isEditing', false);
+			//var record = this.get('model');
+		    //record.save();
+		}
+	},
+
+	isEditing: false,
 });
