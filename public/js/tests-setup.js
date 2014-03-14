be11b125cc763faf4fed1da613ba88049ec07776
David Peddle <dspeddle@gmail.com>
2014-03-14 06:26:15 +0000

    Set application maespace in tests-setup so that previously written tests can pass again.

--- public/js/tests-setup.js
+++ public/js/tests-setup.js
@@ -3,5 +3,12 @@ window.App = Ember.Application.create({
   rootElement: '#qunit-fixture'
 });
 
+App.Store = DS.Store.extend({
+    revision: 12,
+    adapter: DS.RESTAdapter.reopen({
+        namespace: 'api'
+    })
+});
+
 // turn on testing mode
 window.App.setupForTesting();
