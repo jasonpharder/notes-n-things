1d384054940199ecb436801c33f237eec61d5c1a
David Peddle <dspeddle@gmail.com>
2014-03-20 05:07:26 +0000

    Updated Javascript tests to check for various HTML elements upon loading different pages.

--- public/js/tests.js
+++ public/js/tests.js
@@ -14,28 +14,93 @@ test("App Instantiation", function() {
 
 test("Do the templates load correctly", function() {
 	visit("/")
-		.find("ul#course-list")
-		.then(function(header) {
-			equal(header.length, 1, 'found contents');
+		.find("ul#home-sidebar")
+		.then(function(sidebar) {
+			equal(sidebar.length, 1, 'found homepage course sidebar');
 		});
+
+	visit("/")
+		.find("h1#home-header")
+		.then(function(header){
+			equal(header.length, 1, 'found homepage header');
+		});
+
 	visit("/notes")
-		.find("ul#notes-list")
-		.then(function(list) {
-			equal(list.length, 1, 'found notes list');
+		.find("button#create-note")
+		.then(function(button) {
+			equal(button.length, 1, 'found create note button');
 		});
+
+	visit("/notes")
+		.find("div#notes-sidebar")
+		.then(function(sidebar){
+			equal(sidebar.length, 1, 'found notes sidebar');
+		});
+
+	visit("/notes")
+		.find("h1#notes-header")
+		.then(function(header){
+			equal(header.length, 1, "found notes header");
+		});
+
 	visit("/courses")
-		.find("ul#courses-list")
-		.then(function(list) {
-			equal(list.length, 1, 'found courses list');
+		.find("ul#courses-sidebar")
+		.then(function(sidebar) {
+			equal(sidebar.length, 1, 'found courses sidebar');
 		});
+
+	visit("/courses")
+		.find("h1#courses-header")
+		.then(function(header){
+			equal(header.length, 1, "found courses header");
+		});
+
 	visit("/login")
-		.find("form.form-signin")
+		.find("form#signin-form")
 		.then(function(form) {
-			equal(form.length, 1, 'found login');
+			equal(form.length, 1, 'found login form');
 		});
+
+	visit("/login")
+		.find("input#email-field")
+		.then(function(field) {
+			equal(field.length, 1, 'found email field');
+		});
+
+	visit("/login")
+		.find("input#password-field")
+		.then(function(field) {
+			equal(field.length, 1, 'found password field');
+		});
+
+	visit("/login")
+		.find("input#remember-checkbox")
+		.then(function(checkbox) {
+			equal(checkbox.length, 1, 'found remember me checkbox');
+		});
+
+	visit("/login")
+		.find("button#signin-button")
+		.then(function(button) {
+			equal(button.length, 1, "found signin button");
+		});
+
+	visit("/login")
+		.find("button#register-link")
+		.then(function(button) {
+			equal(button.length, 1, "found register link");
+		});
+
 	visit("/users")
-		.find("ul#user-list")
-		.then(function(list) {
-			equal(list.length, 1, 'found user list');
+		.find("ul#users-sidebar")
+		.then(function(sidebar) {
+			equal(sidebar.length, 1, 'found users sidebar');
 		});
+
+	visit("/users")
+		.find("h1#users-header")
+		.then(function(header) {
+			equal(header.length, 1, "found users header");
+		});
+
 });
