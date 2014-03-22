fe7ab89c4158c4e533ba80c35ec1bd2f6c0ff527
kylederrett <derrett.kyle@gmail.com>
2014-03-22 17:13:38 -0500

    Added User class to store user info

---
+++ iosApp/notes-n-things/notes-n-things/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  notes-n-things
+//
+//  Created by Kyle Derrett on 2014-03-22.
+//  Copyright (c) 2014 Comp4350. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
