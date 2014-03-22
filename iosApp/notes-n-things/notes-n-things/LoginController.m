0194cd75c680b004789a5c24a62b5172cefaff51
kylederrett <derrett.kyle@gmail.com>
2014-03-22 14:57:13 -0500

    Added the iOS login screen and controller

---
+++ iosApp/notes-n-things/notes-n-things/LoginController.m
@@ -0,0 +1,38 @@
+//
+//  LoginController.m
+//  notes-n-things
+//
+//  Created by Kyle Derrett on 2014-03-22.
+//  Copyright (c) 2014 Comp4350. All rights reserved.
+//
+
+#import "LoginController.h"
+
+@interface LoginController ()
+
+@end
+
+@implementation LoginController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
