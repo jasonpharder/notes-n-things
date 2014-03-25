8128cf5d13c41a3ff1a0c886739834c5706b078f
Jason Harder <jasonpharder@gmail.com>
2014-03-25 16:06:08 -0500

    added notes controller to index

--- public/js/controllers/note_controller.js
+++ public/js/controllers/note_controller.js
@@ -16,10 +16,9 @@ App.NoteaddController = Ember.ObjectController.extend({
 					});	
 
 					notes.then(function() {
-		                var courseAdd = store.createRecord('course', {
+		                var noteAdd = store.createRecord('note', {
                     		name:      name,
-	    	                alt_name:  altName,
-	        			    professor: userID
+	        			    owner: userID
         				});
 
             			courseAdd.save();
@@ -44,34 +43,7 @@ App.NotesController = Ember.ArrayController.extend({
 			this.set('isEditing', false);
 			//var record = this.get('model');
 		    //record.save();
-		},
-		createNote: function (title, course) {
-			var cookie = document.cookie;
-
-            if (cookie.length != 0) {
-				var cookieUID = cookie.split(';');
-	            var temp = cookieUID[1].split('=');
-	            var userID = temp[1];	
-
-	            if (userID != '1') {
-	            	var notes = DS.PromiseArray.create({
-						promise: this.store.find('note')
-					});
-
-	            	var note = this.store.createRecord('note', {
-	            		file_name: title,
-	            		owner: userID
-	                });
-
-        	        note.save();
-            	} else {
-					document.getElementById('errorMsg').innerHTML = "Guests can not create notes";
-				}
-			} else {
-                console.log("Cannot add message: currently not logged in");
-	            document.getElementById('errorMsg').innerHTML = "Guests can not create notes";
-            }
-  		}
+		}
 	},
 
 	isEditing: false,    
