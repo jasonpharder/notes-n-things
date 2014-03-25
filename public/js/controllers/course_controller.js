90aee97d40fde8bb83cadb041f72939ec9e78a4a
JoshuaDrad <umdrad@cc.umanitoba.ca>
2014-03-25 06:06:59 +0000

    made it so a course name can only be 9 or less characters long

--- public/js/controllers/course_controller.js
+++ public/js/controllers/course_controller.js
@@ -4,60 +4,66 @@ App.CourseaddController = Ember.ObjectController.extend({
             add: function(courseName, courseAltName) {
                 var name = courseName;
                 var altName = courseAltName;
-    			var cookie = document.cookie;			
+		var cookie = document.cookie;			
 
     			if (cookie.length != 0){
-    				var cookieUID = cookie.split(';');
-    				var temp = cookieUID[1].split('=');
-    				var userID = temp[1];
+				if(courseName.length < 10){
+	    				var cookieUID = cookie.split(';');
+    					var temp = cookieUID[1].split('=');
+    					var userID = temp[1];
 
-    				var foundCourse = new Boolean();
-    				foundCourse = false;
+    					var foundCourse = new Boolean();
+    					foundCourse = false;
     			
-    				var store = this.store;
-    				var courses = DS.PromiseArray.create({
-    					promise: this.store.find('course')
-    				});	
+	    				var store = this.store;
+    					var courses = DS.PromiseArray.create({
+    						promise: this.store.find('course')
+    					});	
 
-    				courses.then(function() {
-    					var course = "";
-    					var tempCourseName = "";
-    					var tempStack = new Array();					
+    					courses.then(function() {
+	    					var course = "";
+    						var tempCourseName = "";
+    						var tempStack = new Array();					
     					
-    					course = courses.get('firstObject');
-    					course = courses.popObject();
-    					while (!foundCourse && course != null){
-    						tempCourseName = course.get('name');
-
-    						if (name.localeCompare(tempCourseName) == 0){
-    							foundCourse = true;
-    							tempStack.push(course);
+    						course = courses.get('firstObject');
+    						course = courses.popObject();
+    						while (!foundCourse && course != null){
+	    						tempCourseName = course.get('name');
+	
+    							if (name.localeCompare(tempCourseName) == 0){
+    								foundCourse = true;
+    								tempStack.push(course);
+    							}
+    							else {
+    								tempStack.push(course);
+    								course = courses.popObject();
+	    						}
     						}
-    						else {
-    							tempStack.push(course);
-    							course = courses.popObject();
+    						var temp = tempStack.pop();
+    						while (temp != null) {
+    							courses.pushObject(temp);
+    							temp = tempStack.pop();
+    							
+    						}	
+    						if (foundCourse){
+    							document.getElementById('coursesMsg').innerHTML = "Course name is already being used";
     						}
-    					}
-    					var temp = tempStack.pop();
-    					while (temp != null) {
-    						courses.pushObject(temp);
-    						temp = tempStack.pop();
-    						
-    					}
-    					if (foundCourse){
-    						document.getElementById('coursesMsg').innerHTML = "Course name is already being used";
-    					}
-    					else {
-                        	var courseAdd =store.createRecord('course', {
-    	                        name: name,
-    	                        alt_name: altName,
-    			                professor: userID
-                    		});
-                        	courseAdd.save();
-                       		document.getElementById('coursesMsg').innerHTML = "Successfully added course";
-    					}
-    				});
-    			}else {
+	    					else {
+        			                	var courseAdd =store.createRecord('course', {
+    	                        				name: name,
+				    	                        alt_name: altName,
+    			        			        professor: userID
+                    				});
+                        			courseAdd.save();
+		                       		document.getElementById('coursesMsg').innerHTML = "Successfully added course";
+    						}
+    					});
+    				}
+				else {
+					document.getElementById('coursesMsg').innerHTML = "Course name has to be 9 or less characters long";
+				}	
+			}
+			else {
     				document.getElementById('coursesMsg').innerHTML = "Please register or signin to create a course";
     			}
             }
