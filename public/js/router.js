0e76599a487da09524ef762bc698ff5e2c148d01
Jason Harder <jasonpharder@gmail.com>
2014-03-25 15:44:39 -0500

    added model to noteadd route

--- public/js/router.js
+++ public/js/router.js
@@ -143,3 +143,10 @@ App.NoteRoute = Ember.Route.extend({
 	}
 })
 
+App.NoteaddRoute = Ember.Route.extend({
+  	model: function(){
+    	// the model for this route is a new empty Ember.Object
+    	return Em.Object.create({});
+  	}
+ });
+
